b6702555a0f90aa317bd0aa2028007540cced194
fchoi1 <masterfabster2010@hotmail.com>
2022-01-26 19:58:18 -0500

    fixed seeds

--- models/Bicycle.js
+++ models/Bicycle.js
@@ -3,42 +3,44 @@ const sequelize = require('../config/connection');
 
 class Bicycle extends Model {}
 
-Bicycle.init({
+Bicycle.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
     brand: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     model: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     description: {
-        type: DataTypes.TEXT,
-        allowNull: false
+      type: DataTypes.TEXT,
+      allowNull: false
     },
     stock: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
     price: {
-        type: DataTypes.DECIMAL(6, 2),
-        allowNull: false,
-        validate: {
-            isDecimal: true
-        }
+      type: DataTypes.DECIMAL(6, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
     }
-
-}, {
+  },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'bicycle'
-});
+  }
+);
 
 module.exports = Bicycle;
--- models/Part.js
+++ models/Part.js
@@ -3,34 +3,36 @@ const sequelize = require('../config/connection');
 
 class Part extends Model {}
 
-Part.init({
+Part.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
     type: {
-        type: DataTypes.TEXT,
-        allowNull: false
+      type: DataTypes.TEXT,
+      allowNull: false
     },
     stock: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
     price: {
-        type: DataTypes.DECIMAL(6, 2),
-        allowNull: false,
-        validate: {
-            isDecimal: true
-        }
+      type: DataTypes.DECIMAL(6, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
     }
-
-}, {
+  },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'part'
-});
+  }
+);
 
 module.exports = Part;
