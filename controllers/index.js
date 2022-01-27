bf436e08845fd0fbe2819216c8d1ca220d2250ec
VladimirStarchenko <91164950+VladimirStarchenko@users.noreply.github.com>
2022-01-27 15:49:06 -0500

    controllers conflict resolved

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-const router = require('express').Router()
+const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
 module.exports = router;
-=======
-const router = require('express').Router();
-
-const apiRoutes = require('./api/');
-
-router.use('/api', apiRoutes);
-
-module.exports = router;
->>>>>>> feature/routes
