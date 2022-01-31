add23d12c3ba2d78877986c0343eff9b65e064b2
fchoi1 <masterfabster2010@hotmail.com>
2022-01-31 12:05:00 -0500

    updated homepage routes amd bike handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
+const { Bicycle, Bicycle_Comment, User } = require('../models');
+const resizeArray = require('../utils/resizeArray');
 
 // middleware auth function
 router.use(withAuth);
@@ -28,10 +30,47 @@ router.get('/logout', (req, res) => {
     req.session.destroy(() => {
       res.status(204).end(); // 204 no content, success nothing to do
     });
-    res.render('logout')
+    res.render('logout');
   } else {
-    res.redirect('/');;
+    res.redirect('/');
   }
 });
 
+router.get('/bikes', async (req, res) => {
+  try {
+    const dbBikeData = await Bicycle.findAll({ raw: true, nest: true });
+    if (!dbBikeData) return;
+    const bikeData = resizeArray(dbBikeData, 3);
+    console.log(bikeData);
+    res.render('bikes', { bikeData, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/bikes/:id', async (req, res) => {
+  try {
+    const bike = await Bicycle.findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: Bicycle_Comment,
+          include: { model: User, attributes: ['username'] }
+        }
+      ],
+      raw: true,
+      nest: true
+    });
+    if (!bike) {
+      res.status(404).json({ message: 'No bike found with this id' });
+      return;
+    }
+    console.log(bike);
+    res.render('single-bike', { bike, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
