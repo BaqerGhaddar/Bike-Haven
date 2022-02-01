ae617de64bc84d0f85f901032ee3ba3fb434d2e8
VladimirStarchenko <91164950+VladimirStarchenko@users.noreply.github.com>
2022-01-31 21:36:24 -0500

    wishlist routes added

---
+++ controllers/wishlist-routes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+const { Wishlist } = require('../models');
+const resizeArray = require('../utils/resizeArray');
+
+// middleware auth function
+router.use(withAuth);
+
+router.get('/', async (req, res) => {
+  try {
+    const dbWishlistData = await Wishlist.findOne({
+      where: {
+        user_id: req.session.user_id
+      }
+    });
+    !dbWishlistData
+      ? res.status(404).json({ message: 'No Wishlist found with this id' })
+      : res.json(dbWishlistData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    await Wishlist.create({
+      user_id: req.body.user_id,
+      bike_id: req.body.bike_id
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
