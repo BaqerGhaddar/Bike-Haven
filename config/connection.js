60e9c5a3f29dfbc596143b9caecdc59221604dc8
VladimirStarchenko <91164950+VladimirStarchenko@users.noreply.github.com>
2022-02-02 15:42:10 -0500

    connection error handling

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,15 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // Creating a connection to a database
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
+  }
+);
 
 module.exports = sequelize;
