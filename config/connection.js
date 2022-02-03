15636ee17b826c8d423bed63c73343ecba968d90
Fabulosa <fabiscg79@gmail.com>
2022-02-02 19:27:00 -0500

    delete an error

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+
 // Creating a connection to a database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
