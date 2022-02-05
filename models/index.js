273ddec52d2a28c0a7fcca3531643840bac0aaf0
VladimirStarchenko <91164950+VladimirStarchenko@users.noreply.github.com>
2022-02-05 17:52:01 -0500

    img conflict fix

--- models/index.js
+++ models/index.js
@@ -3,7 +3,6 @@ const Bicycle = require('./Bicycle');
 const Bicycle_Comment = require('./Bicycle-Comment');
 const Part = require('./Part');
 const SubPart = require('./Sub-Part');
-const Image = require('./Image');
 
 User.hasMany(Bicycle_Comment, { foreignKey: 'user_id' });
 Bicycle_Comment.belongsTo(User, { foreignKey: 'user_id' });
@@ -15,14 +14,10 @@ SubPart.belongsTo(Part, {
   foreignKey: 'part_id'
 });
 
-User.hasOne(Image, { foreignKey: 'user_id' });
-Image.belongsTo(User, { foreignKey: 'user_id' });
-
 module.exports = {
   User,
   Bicycle,
   Bicycle_Comment,
   Part,
-  SubPart,
-  Image
+  SubPart
 };
