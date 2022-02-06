a1ed7c8d4d280019c0a95ff55b48f9c8bac38db7
VladimirStarchenko <91164950+VladimirStarchenko@users.noreply.github.com>
2022-02-06 12:07:08 -0500

    conflict fix

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,14 +6,12 @@ const PartCommentRoutes = require('./part-comments-routes');
 const userRoutes = require('./user-routes');
 const partRoutes = require('./part-routes');
 const subRoutes = require('./subPart-routes');
-const wishlistRoutes = require('./wishlist-routes');
 
 router.use('/bikes', bikeRoutes);
 router.use('/bikeComments', bikeCommentRoutes);
 router.use('/partComments', PartCommentRoutes);
 router.use('/users', userRoutes);
 router.use('/customize', partRoutes);
-router.use('/wishlist', wishlistRoutes);
 router.use('/subParts', subRoutes);
 
 module.exports = router;
